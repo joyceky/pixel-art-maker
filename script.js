661ed8a13a8e4f72cb72cb0b07b17e3e67fad068
Kyle Joyce <kyyjoyce@gmail.com>
2016-10-14 17:27:20 -0600

    working iteration 1, attempting to add black starter color

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 "use strict";
 
-var grid = document.querySelector("#myGrid");
+var colorArr = [ '#1ABC9C', '#2ECC71', '#3498DB', '#9B59B6', '#34495E', '#F1C40F', '#E67E22', '#E74C3C', '#BDC3C7', '#7F8C8D'];
 
+var grid = document.querySelector("#myGrid");
+var colors = document.querySelector("#colorWrapper");
+var spans = document.querySelectorAll('span');
+var brushColor = 'black';
+
+function applyColors(colArr){
+    for (var i = 0; i < spans.length; i++) {
+      spans[i].setAttribute("style", "background-color: " + colArr[i]);
+  }
+}
+
+var getColor = function() {
+  if (event.target !== event.currentTarget) {
+    brushColor = event.target.getAttribute("style");
+    console.log(brushColor);
+}};
 
 var changeColor = function() {
     if (event.target !== event.currentTarget) {
-      console.log(event.target);
-     event.target.setAttribute("style", "background-color: black");
+       event.target.setAttribute("style", brushColor);
+}};
 
-  }
-};
-
-  grid.addEventListener("mousedown", changeColor, false);
+applyColors(colorArr);
+grid.addEventListener("mousedown", changeColor, false);
+colors.addEventListener("mousedown", getColor, false);
