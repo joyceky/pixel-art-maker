91e31538b00b03189ff41a0847787f9da117ed85
Kyle Joyce <kyyjoyce@gmail.com>
2016-10-14 12:05:28 -0600

    initial background color change working

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var grid = document.querySelector("#myGrid");
+
+var changeColor = function() {
+    if (event.target !== event.currentTarget) {
+      console.log(event.target);
+     event.target.setAttribute("style", "background-color: black");
+  }
+};
+
+  grid.addEventListener("click", changeColor, false);
